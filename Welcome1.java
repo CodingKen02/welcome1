b8498bc31a790491071443304c078c4035f5bc26
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-01-18 23:09:04 -0600

    Initial commit

---
+++ Welcome1.java
@@ -0,0 +1,27 @@
+/*
+Name: Kennedy Keyes
+Date: February 11, 2021
+Name of Program: Welcome1.java
+Description: This program will print a line of code. 
+ */
+package welcome1;
+
+/**
+ *
+ * @author codingken
+ */
+public class Welcome1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // main method begins execution of Java application
+    public static void main(String[] args) 
+    {
+        // print the sentence "Welcome to Java Programming"
+        System.out.println("Welcome to Java Programming!");
+        
+    } // end method main
+    
+} // end class Welcome1
